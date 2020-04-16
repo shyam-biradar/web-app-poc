ef9039ab3b5be80e23efc98a435166870efbea15
shyam-biradar <shyam.biradar@trilio.io>
2020-04-16 07:19:20 -0400

    Adding first draft

---
+++ src/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Trilio Demo Application</title>
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <h1>Trilio Demo Application</h1>
+  </body>
+</html>
---
+++ src/trilio.php
@@ -0,0 +1,148 @@
+<?php
+
+/**
+ * Use an HTML form to create a new entry in the
+ * users table.
+ *
+ */
+
+
+if (isset($_POST['submit'])) {
+    require "config.php";
+    require "common.php";
+
+    try  {
+        $connection = new PDO($dsn, $username, $password, $options);
+        
+        $new_user = array(
+            "firstname" => $_POST['firstname'],
+            "lastname"  => $_POST['lastname'],
+            "email"     => $_POST['email'],
+            "age"       => $_POST['age'],
+            "location"  => $_POST['location']
+        );
+
+        $sql = sprintf(
+                "INSERT INTO %s (%s) values (%s)",
+                "users",
+                implode(", ", array_keys($new_user)),
+                ":" . implode(", :", array_keys($new_user))
+        );
+        
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+
+<?php require "templates/header.php"; ?>
+
+<?php if (isset($_POST['submit']) && $statement) { ?>
+    <blockquote><?php echo $_POST['firstname']; ?> successfully added.</blockquote>
+<?php } ?>
+
+<h2>Add a user</h2>
+
+<form method="post">
+    <label for="firstname">First Name</label>
+    <input type="text" name="firstname" id="firstname">
+    <label for="lastname">Last Name</label>
+    <input type="text" name="lastname" id="lastname">
+    <label for="email">Email Address</label>
+    <input type="text" name="email" id="email">
+    <label for="age">Age</label>
+    <input type="text" name="age" id="age">
+    <label for="location">Location</label>
+    <input type="text" name="location" id="location">
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+
+<?php require "templates/footer.php"; ?>
+<?php
+
+/**
+ * Function to query information based on 
+ * a parameter: in this case, location.
+ *
+ */
+
+if (isset($_POST['submit'])) {
+    try  {
+        
+        require "config.php";
+        require "common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT * 
+                        FROM users
+                        WHERE location = :location";
+
+        $location = $_POST['location'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+        
+<?php  
+if (isset($_POST['submit'])) {
+    if ($result && $statement->rowCount() > 0) { ?>
+        <h2>Results</h2>
+
+        <style>
+        table, th, td {
+          border: 1px solid black;
+        }
+        </style>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email Address</th>
+                    <th>Age</th>
+                    <th>Location</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php foreach ($result as $row) { ?>
+            <tr>
+                <td><?php echo escape($row["id"]); ?></td>
+                <td><?php echo escape($row["firstname"]); ?></td>
+                <td><?php echo escape($row["lastname"]); ?></td>
+                <td><?php echo escape($row["email"]); ?></td>
+                <td><?php echo escape($row["age"]); ?></td>
+                <td><?php echo escape($row["location"]); ?></td>
+                <td><?php echo escape($row["date"]); ?> </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+    <?php } else { ?>
+        <blockquote>No results found for <?php echo escape($_POST['location']); ?>.</blockquote>
+    <?php } 
+} ?> 
+
+<h2>Find user based on location</h2>
+
+<form method="post">
+    <label for="location">Location</label>
+    <input type="text" id="location" name="location">
+    <input type="submit" name="submit" value="View Results">
+</form>
+
+
+<?php require "templates/footer.php"; ?>
